4016bd032e8c5878635db57efd294e77889bda16
Gustavo <g.boanerges2@gmail.com>
2019-03-05 17:05:02 -0300

    Add edit advertence page

--- pfc/views/pages/advertence_update.php
+++ pfc/views/pages/advertence_update.php
@@ -2,32 +2,62 @@
 <head>
 	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<title>Cadastro de Advertências</title>
+	<title>Editar Advertência</title>
 	<?php $this->loadCSS()?>
     <link href="https://unpkg.com/gijgo@1.9.11/css/gijgo.min.css" rel="stylesheet" type="text/css" />
 </head>
 
 <body class="d-flex flex-column">
 	<?php $this->loadHeader()?> 
-
-	<main>
-		<div class="container mt-4 flex-grow">
-			<div class="row">
-				<div class="d-none d-sm-block col-md-3 col-lg-4"></div>
-				<div class="col-md-6 col-12 col-lg-4">
-					
-					<form id="form" class="form-group" method="POST" enctype="multipart/form-data">
-						<h2>Editar Advertência</h2>
-                        
-                        
-					</form>
-
-
-				</div>
-				<div class="d-none d-sm-block col-md-3 col-lg-4"></div>
-			</div>
-		</div>
-	</main>
+	
+	<div class="container mt-4 flex-grow">
+        <div class="row">
+            <div class="col-12">
+                <h3>Editar Advertência</h3><br>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 text-center">
+                <div class="row">
+                    <div class="col-12">
+                        <table id="advertences" class="table">
+                            <thead class="thead-light text-center">
+								<tr>
+									<th scope="col">Membro</th>
+									<th scope="col">Motivo</th>
+									<th scope="col">Data</th>
+									<th scope="col">Pontos</th>
+									<th scope="col">Responsável</th>
+									<th scope="col">Deferida</th>
+									<th scope="col">&nbsp</th>
+								</tr>
+                            </thead>
+                            <tbody class="text-center" id="project-body">
+								<?php
+									if(isset($this->data['advertencesList'])){
+										foreach($this->data['advertencesList'] as $advertences){                                            
+											echo 
+												'<tr>
+													<td>'.$advertences->getMemberName().'</td>
+													<td>'.$advertences->getReason().'</td>
+													<td>'.$advertences->getDate().'</td>	
+													<td>'.$advertences->getPoints().'</td>	
+													<td>'.$advertences->getResponsible().'</td>														
+													<td>'.$advertences->getDefense().'</td>														
+													<td><button class=""><i class="fas fa-edit"></i></button><button class=""><i class="fas fa-trash"></i></button></td>														
+												</tr>';
+											
+										}
+									}
+                                ?>
+                            </tbody>
+                        </table>
+                        <div class="col-md-3"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+	</div>
 
 	<?php $this->loadFooter()?>	
 	<?php $this->loadJavascript()?>
@@ -35,9 +65,9 @@
 	<script src="<?php $this->path('assets/js/advertence_register.js')?>"></script>
 	<script src="https://unpkg.com/gijgo@1.9.11/js/gijgo.min.js" type="text/javascript"></script>
 	<script>
-        $('#datepicker').datepicker({
-            uiLibrary: 'bootstrap4'
-        });
+        $(document).ready( function () {
+    		$('#advertences').DataTable();
+		} );
     </script>
 </body>
 </html>
